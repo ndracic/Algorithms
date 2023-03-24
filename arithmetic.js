d4b9b4f1fa32d9553a55b25e28fca76287fa9589
Nerms <Nerms@Nermins-MacBook-Pro.local>
2023-03-24 18:36:49 -0500

    returned values for functions and added test file

--- arithmetic.js
+++ arithmetic.js
@@ -1,7 +1,15 @@
-var add = function(num1, num2) {};
+var add = function(num1, num2) {
+    return num1 + num2
+}; 
 
-var subtract = function(num1, num2) {};
+var subtract = function(num1, num2) {
+    return num1 - num2
+};
 
-var multiply = function(num1, num2) {};
+var mulitply = function(num1, num2) {
+    return num1 * num2
+};
 
-var divide = function(num1, num2) {};
+var divide = function (num1, num2) {
+    return num1 / num2
+};
